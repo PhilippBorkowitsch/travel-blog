81170614d2c2f621cdb774132444759c7246c46f
Andrea Speidel <andrea.speidel@hpe.com>
2020-01-22 15:42:37 +0000

    Fixed Image

--- src/connectors/image.connector.ts
+++ src/connectors/image.connector.ts
@@ -2,63 +2,61 @@ import { Request, Response, NextFunction } from "express";
 import { Image } from "../models/Image.model";
 
 export const postImage = (req: Request, res: Response, next: NextFunction) => {
-    const image = new Image({
-        imageName: req.body.imageName,
-        description: req.body.description,
-        postId: req.body.postId,
-    });
+  const image = new Image({
+    imageName: req.body.imageName,
+    description: req.body.description,
+    postId: req.body.postId
+  });
 
-    image.save().then(() => {
-        res
-            .status(200)
-            .json(image);
-    }).catch((err) => {
-        res
-            .status(400)
-            .json(err);
+  image
+    .save()
+    .then(() => {
+      res.status(200).json(image);
+    })
+    .catch(err => {
+      res.status(400).json(err);
     });
 };
 
 export const getImages = (req: Request, res: Response, next: NextFunction) => {
-    Image.findAll().then((images) => {
-        res
-            .status(200)
-            .json(images);
-    }).catch((err) => {
-        res
-            .status(400)
-            .json(err);
+  Image.findAll()
+    .then(images => {
+      res.status(200).json(images);
+    })
+    .catch(err => {
+      res.status(400).json(err);
     });
 };
 
 export const getImage = (req: Request, res: Response, next: NextFunction) => {
-    Image.findOne({
-        where: {
-            id: req.body.id,
-        }
-    }).then((image) => {
-        res
-            .status(200)
-            .json(image);
-    }).catch((err) => {
-        res
-            .status(400)
-            .json(err);
+  Image.findOne({
+    where: {
+      id: req.body.id
+    }
+  })
+    .then(image => {
+      res.status(200).json(image);
+    })
+    .catch(err => {
+      res.status(400).json(err);
     });
 };
 
-export const getImagesOfPost = (req: Request, res: Response, next: NextFunction) => {
-    Image.scope('full').findOne({
-        where: {
-            postId: req.params.postId,
-        }
-    }).then((image) => {
-        res
-            .status(200)
-            .json(image);
-    }).catch((err) => {
-        res
-            .status(400)
-            .json(err);
+export const getImagesOfPost = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  Image.scope("full")
+    .findAll({
+      where: {
+        postId: req.params.postId
+      }
+    })
+    .then(image => {
+      res.status(200).json(image);
+    })
+    .catch(err => {
+      res.status(400).json(err);
     });
 };
